7f732e14beea924215152bfacd7b0af178826a8a
Madushani95 <60032653+Madushani95@users.noreply.github.com>
2020-05-10 02:14:26 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,40 @@
+
+<div style="padding-top:10px;">
+			<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+			
+			<!-- Toggler/collapsibe Button -->
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+				<span class="navbar-toggler-icon"></span>
+		    </button>
+
+		    <!-- Navbar links -->
+		    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+			
+			
+			  <!-- Links -->
+			  <ul class="navbar-nav">
+				<li class="nav-item">
+				  <a class="nav-link" href="index.php">Home</a>
+				</li>
+				
+				<!-- Dropdown -->
+				<li class="nav-item dropdown">
+				  <a class="nav-link dropdown-toggle" href="#" id="navbardrop1" data-toggle="dropdown">
+					Subjects
+				  </a>
+				  <div class="dropdown-menu">
+					<a class="dropdown-item" href="Physics.php">Physics</a>
+					<a class="dropdown-item" href="Chemistry.php">Chemistry</a>
+					<a class="dropdown-item" href="CombinedM.php">Combined Maths</a>
+					<a class="dropdown-item" href="Bio.php">Biology</a>
+					<a class="dropdown-item" href="ICT.php">ICT</a>
+					<a class="dropdown-item" href="GE.php">General English</a>
+                    <a class="dropdown-item" href="GIT.php">GIT</a>
+				  </div>
+				</li>
+            
+			
+			  </ul>
+			  </div>
+			</nav> 
+			</div>
---
+++ schedules.php
@@ -0,0 +1,42 @@
+<?php
+function u1v1(){
+    $summary = '01. Introduction to physics';
+    $description = 'Nanasa TV';
+    $start = '2020-05-19T16:00:00+05:30';
+    $end = '2020-05-19T18:00:00+05:30';
+    include 'calendar.php';
+}
+
+function u1v2(){
+    $summary = '02. Physical quantities and unit';
+    $description = 'Nanasa TV';
+    $start = '2020-05-26T16:00:00+05:30';
+    $end = '2020-05-26T18:00:00+05:30';
+    include 'calendar.php';
+}
+
+function u1v3(){
+    $summary = '03. Dimensions';
+    $description = 'Nanasa TV';
+    $start = '2020-06-02T16:00:00+05:30';
+    $end = '2020-06-02T18:00:00+05:30';
+    include 'calendar.php';
+}
+
+function u1v4(){
+    $summary = '04. Measuring instrumen';
+    $description = 'Nanasa TV';
+    $start = '2020-06-09T16:00:00+05:30';
+    $end = '2020-06-09T18:00:00+05:30';
+    include 'calendar.php';
+}
+
+function u1v5(){
+    $summary = '05. Scalars and vectors';
+    $description = 'Nanasa TV';
+    $start = '2020-06-16T16:00:00+05:30';
+    $end = '2020-06-16T18:00:00+05:30';
+    include 'calendar.php';
+}
+
+?>
