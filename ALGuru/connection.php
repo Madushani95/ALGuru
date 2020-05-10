99735f48d507c51d2bd66992a5a8c71bf65447bf
Madushani95 <60032653+Madushani95@users.noreply.github.com>
2020-05-11 00:43:38 +0530

    Add existing file

---
+++ ALGuru/connection.php
@@ -0,0 +1,131 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+	
+.button {
+  display: inline-block;
+  border-radius: 8px;
+  background-color: #008CBA;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 22px;
+  padding: 5px 12px;
+  width: 175px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+</style>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('ALGuru');
+    $client->setScopes(Google_Service_Calendar::CALENDAR);
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+          
+
+            if(!credentials_in_browser()) {
+                
+                $authUrl = $client->createAuthUrl();
+               
+                     
+              return "<p>Sign to use this service!</p><a href='$authUrl'><button class='button' style='vertical-align:middle'><i class='fa fa-google' style='padding:0px 20px 5px 5px'></i>
+				<span> Sign In </span></button></a>";
+                
+            }
+   
+            $authCode = $_GET['code'];
+            
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+            
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+function credentials_in_browser() {
+    if(isset($_GET['code'])) return true;
+
+    return false;
+}
+
+$client = getClient();
+
+if(! is_a ($client, "Google_Client")) {
+        echo $client;
+}
+else { 
+        
+           include 'content.php'   ; 
+            
+    
+    }
+
+?>
+
---
+++ ALGuru/content.php
@@ -0,0 +1,150 @@
+<div class="container">
+          
+         
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#l1" role="tab" aria-controls="l1">Lesson 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#l2" role="tab" aria-controls="l2">Lesson 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#l3" role="tab" aria-controls="l3">Lesson 3</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#l4" role="tab" aria-controls="l4">Lesson 4</a>
+          </li>
+        </ul>
+
+        
+                 
+        <div class="tab-content">
+          <div class="tab-pane active p-3" id="l1" role="tabpanel">
+            <h3>Measurement</h3>
+             <ul class="list-group">
+              <li class="list-group-item"> 
+                   <h5 class="list-group-item-heading">01. Introduction to physics</h5>
+                    <p class="list-group-item-text pl-5">19.05.2020 at 4.00pm on Nanasa TV<br>
+                       <button id="submit" name="submit" type="button" class="btn btn-outline-info btn-sm" onclick='location.href="?submit=t1"'>Add to Calendar</button>
+                  
+                    </p>
+                   
+                  </li> 
+              <li class="list-group-item">
+                 
+                  <h5 class="list-group-item-heading">02. Physical quantities and unit</h5>
+                    <p class="list-group-item-text pl-5">26.05.2020 at 4.00pm on Nanasa TV<br>
+                        <button id="submit" name="submit" type="button" class="btn btn-outline-info btn-sm" onclick='location.href="?submit=t2"'>Add to Calendar</button>
+                    </p>
+                 </li>
+              <li class="list-group-item">
+                 <h5 class="list-group-item-heading">03. Dimensions</h5>
+                    <p class="list-group-item-text pl-5">02.06.2020 at 4.00pm on Nanasa TV<br>
+                       <button id="submit" name="submit" type="button" class="btn btn-outline-info btn-sm" onclick='location.href="?submit=t3"'>Add to Calendar</button>
+                    </p>
+                 </li>
+                 <li class="list-group-item">
+                 <h5 class="list-group-item-heading">04. Measuring instrumen</h5>
+                    <p class="list-group-item-text pl-5">09.06.2020 at 4.00pm on Nanasa TV<br>
+                       <button id="submit" name="submit" type="button" class="btn btn-outline-info btn-sm" onclick='location.href="?submit=t4"'>Add to Calendar</button>
+                    </p>
+                 </li>
+                 <li class="list-group-item">
+                 <h5 class="list-group-item-heading">05. Scalars and vectors</h5>
+                    <p class="list-group-item-text pl-5">16.06.2020 at 4.00pm on Nanasa TV<br>
+                        <button id="submit" name="submit" type="button" class="btn btn-outline-info btn-sm" onclick='location.href="?submit=t5"'>Add to Calendar</button>
+                    </p>
+                 </li>
+               
+                 
+            </ul> 
+        </div>
+          <div class="tab-pane p-3" id="l2" role="tabpanel">
+            
+              <h3>Mechanics</h3>
+               <ul class="list-group">
+                <li class="list-group-item">01. Kinematics</li>
+                <li class="list-group-item">02. Resultant force and moment of force</li>
+                <li class="list-group-item">03. Force and motion</li>
+                <li class="list-group-item">04. Equilibrium</li>
+                <li class="list-group-item">05. Work, energy and power</li>
+                <li class="list-group-item">06. Rotational motion</li>
+                <li class="list-group-item">07. Fluid-dynamics</li>
+              </ul>
+            </div>
+          <div class="tab-pane p-3" id="l3" role="tabpanel">
+              <h3>Oscillations and Wave</h3>
+            <ul class="list-group">
+                <li class="list-group-item">01. NYA</li>
+                <li class="list-group-item">02. NYA</li>
+                <li class="list-group-item">03. NYA</li>
+                <li class="list-group-item">04. NYA</li>
+                <li class="list-group-item">05. NYA</li>
+                <li class="list-group-item">06. NYA</li>
+                <li class="list-group-item">07. NYA</li>
+              </ul>
+            </div>
+          <div class="tab-pane p-3" id="l4" role="tabpanel">
+              <h3>Thermal Physics</h3>
+            <ul class="list-group">
+                <li class="list-group-item">01. NYA</li>
+                <li class="list-group-item">02. NYA</li>
+                <li class="list-group-item">03. NYA</li>
+                <li class="list-group-item">04. NYA</li>
+                <li class="list-group-item">05. NYA</li>
+                <li class="list-group-item">06. NYA</li>
+                <li class="list-group-item">07. NYA</li>
+              </ul>
+            </div>
+        </div>
+           
+		</div>
+
+
+ 
+<?php
+    
+  
+
+if($_GET){
+    include 'schedules.php';
+    if(isset($_GET['submit'])){
+        
+       $req = $_GET['submit'];
+        switch ($req) {
+            case 't1':
+                u1v1();
+                break;
+            case 't2':
+                u1v2();
+                break;
+            case 't3':
+                u1v3();
+                break;
+            case 't4':
+                u1v4();
+                break;
+            case 't5':
+                u1v5();
+                break;
+            default:
+                echo "Error!";
+        }
+      
+    }
+    
+    
+}
+
+
+   
+    
+
+       
+
+
+
+
+
+
+?>
